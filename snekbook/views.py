00823886320d97846671ba71b99ce942993ffd01
jeremyfisher <Jeremy.Fisher@bd.com>
2019-03-04 20:48:59 -0800

    broke up index to use inheritance

--- snekbook/views.py
+++ snekbook/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'snekbook/base.html', {})
+    return render(request, 'snekbook/index.html', {})
--- supersnekbook/urls.py
+++ supersnekbook/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('snekbook/', include('snekbook.urls')),
+    path('', include('snekbook.urls')),
     path('admin/', admin.site.urls),
 ]
