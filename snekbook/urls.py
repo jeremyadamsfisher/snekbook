743b805a0b2dd9a93f152a9c85fe36486cda062a
jeremyfisher <Jeremy.Fisher@bd.com>
2019-03-10 15:35:58 -0700

    content-based recommendations are working

--- snekbook/urls.py
+++ snekbook/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("<int:snake_id>/", views.detail, name="detail"),
+]
--- snekbook/views.py
+++ snekbook/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-
+from .models import Snake
 
 def index(request):
-    snakes = [
-        {"name": "Boa Constrictor", "stars": 3},
-        {"name": "Sneky snek", "stars": 4}
-    ]
-    return render(request, "snekbook/index.html", {"snake_number": len(snakes), "snakes": snakes})
+    return render(
+        request,
+        "snekbook/index.html",
+        {
+            "snake_number": 2
+        }
+    )
+
+def detail(request, snake_id):
+    snake = Snake.objects.get(pk=snake_id)
+    return render(
+        request,
+        "snekbook/detail.html",
+        {
+            "snake": snake,
+            "recommended_snakes": snake.recommended_snakes.all()
+        }
+    )
