41121c98dbe1ee4e4c8a9489972ef983d6fdbb62
Jeremy Fisher <jeremyfisher@Jeremys-MacBook-Pro.local>
2019-06-15 09:55:11 -0700

    move snekbook into apps dir

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "snekbook",
+    "apps.snekbook",
 ]
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
