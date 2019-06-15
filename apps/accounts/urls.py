b329413ca10a44440dbfb74107474592e7301866
Jeremy Fisher <jeremyfisher@Jeremys-MacBook-Pro.local>
2019-06-15 11:11:46 -0700

    basic implementation of the registration form

---
+++ apps/accounts/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('signup/', views.Register.as_view(), name='signup'),
+]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
 
-# Create your views here.
+from . import forms
+
+class Register(generic.CreateView):
+    form_class = forms.CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
