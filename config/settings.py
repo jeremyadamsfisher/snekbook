24cdde42a38f56d60173e0fd507cdb506b79fe95
Jeremy Fisher <jeremyfisher@Jeremys-MacBook-Pro.local>
2019-06-15 09:57:13 -0700

    clean up dead code

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "1s*hswqz(91_q5g6ci!c&33&5xr$ua2_!8(q3c#%a=8h_eo$xq"
 DEBUG = True
-ALLOWED_HOSTS = ["snek-env.an2pgtke5t.us-west-2.elasticbeanstalk.com", "*"]
+ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -46,9 +46,7 @@ DATABASES = {
     }
 }
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
-    },
+    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
@@ -58,12 +56,8 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-
 STATIC_URL = "/static/"
-
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
-
 AUTH_USER_MODEL = "snekbook.User"
-
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
