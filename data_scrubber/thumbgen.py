3503be35534e316bbde19199ee64e087d73c6df0
Jeremy Fisher <jeremy@adamsfisher.me>
2019-06-16 11:56:08 -0700

    first round of final images

---
+++ data_scrubber/thumbgen.py
@@ -0,0 +1,19 @@
+from PIL import Image
+from pathlib import Path
+
+def main():
+    images_orig = Path("curated_downloads").glob("*")
+    images_new_dir = Path("thumbs")
+    images_new_dir.mkdir(exist_ok=True)
+    for img_fp in images_orig:
+        for im_type, size in [("norm", (300, 300)), ("thumb", (50, 50))]:
+            with Image.open(img_fp) as im:
+                main_dim = min(im.size)
+                im.convert('RGB')
+                im_cropped = im.crop((0, 0, main_dim, main_dim))
+                im_cropped.thumbnail(size, Image.ANTIALIAS)
+                thumb_path = images_new_dir / f"{im_type}_{img_fp.stem}.jpg"
+                im_cropped.save(str(thumb_path), 'JPEG', quality=80)
+
+if __name__ == "__main__":
+    main()
